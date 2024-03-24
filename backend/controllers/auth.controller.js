e16364feab03ec4e94f245d56c6980cbcfb3e0ac
NibidaGhimire <nibidaghimire39@gmail.com>
2024-03-24 15:42:19 +0545

    login, logout controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -51,12 +51,39 @@ export const signup = async(req,res)=>{
     }
 };
 
-export const login = (req,res)=>{
-    console.log("Login User");
+export const login = async(req,res)=>{
+    try{
+        const {username,password}=req.body;
+        const user= await User.findOne({username});
+        const isPasswordCorrect = await bcryprt.compare(password,user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error:"Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            username:user.username,
+            profilePic:user.profilePic
+        });
+
+    }catch(error){
+        console.log("Error in login controller",error.message)
+        res.status(500).json({error:"Internal Server Error"})
+    }
 }
 
 export const logout = (req,res)=>{
-    console.log("Logout User");
+    try{
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message:"Logged out successfully"});
+    }catch(error){
+        console.log("Error in logout controller",error.message)
+        res.status(500).json({error:"Internal Server Error"})
+    }
 }
 
 
